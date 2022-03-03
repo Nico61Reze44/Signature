571aa1a7f3545ad4bceeb0f5d7b4e962c5e14fd6
Nico61Reze44 <60397339+Nico61Reze44@users.noreply.github.com>
2022-03-03 06:31:01 +0100

    test2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> aXXXXX.
+          Edit <code>src/App.js</code> TEST.
         </p>
         <a
           className="App-link"
